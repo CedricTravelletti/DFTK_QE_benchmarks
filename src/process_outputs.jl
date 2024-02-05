255d713b31e6da50dab7f03e98f7882248bc9cf6
CedricTravelletti <cedrictravelletti@gmail.com>
2024-02-05 14:49:31 +0100

    Parse QE wall time.

--- src/process_outputs.jl
+++ src/process_outputs.jl
@@ -1,33 +1,49 @@
 using LazyArtifacts
 
 
-function process_QE_output(QE_output_string)
-	QE_iterations = parse.(Int64, split(
+function process_QE_output(QE_output_string; computation="scf")
+	iterations = parse.(Int64, split(
 			    read(
 			        pipeline(
 		`echo $QE_output_string`,
 		`sed -n '/convergence has been achieved/s/.[^0-9]*\([0-9]\+\).*/\1/p'`), String),
 			    '\n')[1:end-1]) # Remove last empty character.
-	QE_n_kpoints = parse(Int64, split(
+	n_kpoints = parse(Int64, split(
 			    read(
 			        pipeline(
 		`echo $QE_output_string`,
 		`sed -n '/number of k points=/s/.[^0-9]*\([0-9]\+\).*/\1/p'`), String),
 			    '\n')[1])
-	QE_optim_steps = parse(Int64, split(
-			    read(
-			        pipeline(
-		`echo $QE_output_string`,
-		`sed -n '/.*\([0-9]\+\) bfgs steps*/p'`), String),
-			    '\n')[1])
-	QE_symmetry_metadata = read(pipeline(
+	symmetry_metadata = read(pipeline(
 		`echo $QE_output_string`,
 		`sed -n '/Sym\. Ops\./p'`), String)
-	QE_n_G_vectors = parse.(Int64, split(
+	n_G_vectors = parse.(Int64, split(
 			    read(
 			        pipeline(
 		`echo $QE_output_string`,
 	       `sed -n 's/.[^0-9]*\([0-9]\+\) G-vectors.*/\1/p'`), String),
 			    '\n')[1])
-	(; QE_iterations, QE_n_kpoints, QE_optim_steps, QE_symmetry_metadata, QE_n_G_vectors)
+	wall_time_str = read(pipeline(
+		`echo $QE_output_string`,
+		`sed -n 's/.*PWSCF.[^0-9]*\(.*\) CPU.[^0-9]*\(.*\) WALL$/\2/p'`), String)
+	# Extract minutes and seconds.
+	matched = match(r"(?:(\d+)h)?(\d+)m(\d+\.\d+)s", wall_time_str)
+	minutes = parse(Int, matched.captures[2])
+	seconds = parse(Float64, matched.captures[3])
+	if matched.captures[1] !== nothing
+		hours = parse(Int, matched.captures[1])
+	else hours = 0
+	end
+	wall_time = 60 * 60 * hours + 60 * minutes  + seconds
+
+	if computation == "relax"
+		optim_steps = parse(Int64, split(
+			    read(
+			        pipeline(
+		`echo $QE_output_string`,
+		`sed -n '/.*\([0-9]\+\) bfgs steps*/p'`), String),
+			    '\n')[1])
+	else optim_steps = nothing
+	end
+	(; iterations, n_kpoints, optim_steps, symmetry_metadata, n_G_vectors, wall_time)
 end
